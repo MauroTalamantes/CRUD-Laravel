544fe16d3926907a972de56985e85952cbdcb5d4
Kirlian MTV <maurovillagrana33@gmail.com>
2022-10-22 15:39:09 -0500

    First commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $clients = Client::paginate(5);
+        return view('client.index')->with('clients', $clients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('client.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:50',
+            'due' => 'required|gte:1'
+        ]);
+
+        $client = Client::create($request->only('name', 'due', 'comments'));
+
+        return redirect()->route('client.index')->with('mensaje', 'Cliente creado exitosamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client $client)
+    {
+        return view('client.form')->with('client', $client);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $request->validate([
+            'name' => 'required|max:50',
+            'due' => 'required|gte:1'
+        ]);
+
+        $client->name = $request->name;
+        $client->due = $request->due;
+        $client->comments = $request->comments;
+        $client->save();
+        return redirect()->route('client.index')->with('mensaje', 'Cliente editado exitosamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+        Session::flash('mensaje', 'Cliente eliminado exitosamente');
+        return redirect()->route('client.index');
+    }
+}
